3c9733f52d47fa1179e0c303870c5dbf90bdae4e
LuckyPuppy514 <122154133@qq.com>
2022-06-18 12:06:08 +0800

    代码重构：删除原代码

--- package.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-call mvn clean package -Dmaven.test.skip=true
-xcopy "target\jproxy*.jar" "release\jproxy.jar"
-copy "target\classes\application.yml" "release\application.yml"
-copy "target\classes\application-prod.yml" "release\application-prod.yml"
-copy "target\classes\logback-spring.xml" "release\logback-spring.xml"
