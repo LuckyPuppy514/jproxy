17da35a4ca6f9d7117fdc8da35920b6bdc6b3215
li qiancheng <842803916@qq.com>
2023-04-08 22:20:41 +0800

    添加一些个配置的默认值

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,8 @@
 # 初始化持久化目录
 CONFIG_PATH=/app/config
 DATABASE_PATH=/app/database
+
+
 if [ ! -d "${CONFIG_PATH}" ]; then
   mkdir -p ${CONFIG_PATH}
 fi
@@ -16,10 +18,11 @@ cp -n /app/resources/application-prod.yml ${CONFIG_PATH}/application-prod.yml
 cp -n /app/resources/database/jproxy.db ${DATABASE_PATH}/jproxy.db
 
 # 设置权限
-chown -R ${PUID}:${PGID} /app/
+chown -R ${PUID-1000}:${PGID-1000} /app/
 umask ${UMASK}
 
 # 启动应用
+
 cd /app
 dpkg -s gosu >/dev/null 2>&1 || (apt update && apt-get -y install gosu)
-gosu ${PUID}:${PGID} java ${JAVA_OPTS} -Dfile.encoding=utf-8 -Dspring.config.location=${CONFIG_PATH}/ -cp $( cat /app/jib-classpath-file ) $( cat /app/jib-main-class-file )
+gosu ${PUID-1000}:${PGID-1000} java ${JAVA_OPTS--Xms512m -Xmx512m} -Dfile.encoding=utf-8 -Dspring.config.location=${CONFIG_PATH}/ -cp $( cat /app/jib-classpath-file ) $( cat /app/jib-main-class-file )
