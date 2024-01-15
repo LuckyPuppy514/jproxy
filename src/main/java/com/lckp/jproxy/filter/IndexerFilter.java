fa048aedfc85f1b39385dd0ae1515ee5412e0800
LuckyPuppy514 <53246532+LuckyPuppy514@users.noreply.github.com>
2024-01-15 09:12:28 +0800

    校正 Prowlarr 分页 offset

--- src/main/java/com/lckp/jproxy/filter/IndexerFilter.java
+++ src/main/java/com/lckp/jproxy/filter/IndexerFilter.java
@@ -99,7 +99,9 @@ public abstract class IndexerFilter extends BaseFilter {
 				if (count > indexerRequest.getLimit()) {
 					count = indexerRequest.getLimit();
 					newXml = XmlUtil.remove(newXml, count);
-					offset = 0 - count;
+					if (index > 1) {
+						offset = offsetList.get(index - 1) - count;
+					}
 				}
 				if (count > 0 || xml == null) {
 					xml = XmlUtil.merger(xml, newXml);
